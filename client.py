d3eb30fc738b903cf296fe0f90f22d8d9852e82d
sperezmaz <s.perezmaz@alumnos.urjc.es>
2017-11-27 00:59:25 +0100

    semi-acabado

--- client.py
+++ client.py
@@ -4,10 +4,9 @@
 import sys
 import socket
 
-metodos = ['invite', 'ack', 'bye']
 # Constantes. Dirección IP del servidor y contenido a enviar
 try:
-    METODO = sys.argv[1]
+    METODO = sys.argv[1].upper()
     address_port = sys.argv[2]
     NAME = address_port.split('@')[0]
     resto = address_port.split('@')[1] 
@@ -17,14 +16,21 @@ try:
 except:
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
-line1 = METODO.upper() + " sip:" + NAME + "@" + SERVER + ' SIP/2.0\r\n'
-line = line1 + '\r\n\r\n'
+line = METODO + " sip:" + NAME + "@" + SERVER + " SIP/2.0\r\n\r\n"
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PUERTO))
-    print("Enviando:", line)
-    my_socket.send(bytes(line, 'utf-8'))  # lo pasamos a bytes
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))  # pasa a string los bytes
-
+    if METODO == "INVITE" or METODO == "BYE":
+        print("Enviando:", line)
+        my_socket.send(bytes(line, 'utf-8'))  # lo pasamos a bytes
+        data = my_socket.recv(1024)
+        condicion1 = data.decode('utf-8').split(" ")[-1]
+        if condicion1 == "OK\r\n\r\n" and METODO != "BYE":
+            print('Recibido -- ', data.decode('utf-8'))
+            line_ack = "ACK sip:" + NAME + "@" + SERVER + " SIP/2.0\r\n\r\n"
+            my_socket.send(bytes(line_ack, 'utf-8'))
+            data = my_socket.recv(1024)
+            print('Recibido -- ', data.decode('utf-8'))
+    else:
+        print("Método no válido")
 print("Socket terminado.")
--- server.py
+++ server.py
@@ -5,6 +5,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+import os
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -14,18 +16,41 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
+        line = self.rfile.read()
+        print("El cliente nos manda " + line.decode('utf-8'))
+        message = line.decode('utf-8').split()
+        metodo = message[0]
+        ip_client = self.client_address[0]
+        puerto_client = self.client_address[1]
+        if metodo == "INVITE":
+            self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+            self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        elif metodo == "ACK":
+            # aEjecutar es un string con lo que se ha de ejecutar en la shell
+            aEjecutar1 = 'mp32rtp -i ' + ip_client + '-p ' + puerto_client 
+            aEjecutar = aEjecutar1 + '< ' + FICHERO_AUDIO
+            print("Vamos a ejecutar", aEjecutar)
+            os.system(aEjecutar)
+        elif metodo == "BYE":
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        elif metodo not in ["INVITE", "ACK", "BYE"]:
+            self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+        else:
+            self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
 
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
 
 if __name__ == "__main__":
+    try:
+        IP = sys.argv[1]
+        PORT_SERV = int(sys.argv[2])
+        FICHERO_AUDIO = sys.argv[3]
+    except:
+        sys.exit("Usage: python3 server.py IP port audio_file")
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer((IP, PORT_SERV), EchoHandler)
     print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    try:
+        serv.serve_forever()  # espera en un bucle
+    except KeyboardInterrupt:  # ^C
+        print("Finalizado servidor")
